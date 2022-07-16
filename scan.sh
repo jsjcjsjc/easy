6b8fd33ece3d7c7f2eaad71176ebced1ff0b1905
jsjcjsjc <jsjcjsjc@users.noreply.github.com>
2022-07-16 10:40:45 +0800

    Add files via upload

---
+++ scan.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+apt-get update && apt-get install -y curl wget unzip screen nginx && \
+cd ~ && \
+curl -sSL https://get.docker.com | sh && \
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+chmod +x /usr/local/bin/docker-compose && \
+rm -rf domain-scan-docker && \
+wget https://cloud.g60.net/cloud/domain/domain-scan-docker.zip && \
+unzip domain-scan-docker.zip && \
+rm domain-scan-docker.zip && \
+cd domain-scan-docker && \
+cp *.conf /etc/nginx/conf.d/ && \
+sed -i '1c user root;' /etc/nginx/nginx.conf && \
+service docker start && \
+docker-compose build && \
+docker-compose up -d && \
+service nginx restart && \
+touch ~/domain-scan-docker.ok
