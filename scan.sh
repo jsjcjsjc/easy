5762f6e4d09613dba1cf1782a6b425a3142663df
jsjcjsjc <jsjcjsjc@users.noreply.github.com>
2022-07-16 10:51:11 +0800

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -5,6 +5,7 @@ curl -sSL https://get.docker.com | sh && \
 curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
 chmod +x /usr/local/bin/docker-compose && \
 rm -rf domain-scan-docker && \
+rm -rf domain-scan-docker.zip && \
 wget https://cloud.g60.net/d/cloud/domain/domain-scan-docker.zip && \
 unzip domain-scan-docker.zip && \
 rm domain-scan-docker.zip && \
