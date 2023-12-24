ed9d27593aa212af755f2d183e02dee08054baec
jsjcjsjc <jsjcjsjc@users.noreply.github.com>
2023-12-24 22:45:30 +0800

    Update vps_us.sh

--- vps_us.sh
+++ vps_us.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-安装必要组件
+# 安装必要组件
 apt-get update
 apt-get install -y curl sed util-linux coreutils
 
@@ -9,26 +9,29 @@ dd if=/dev/zero of=/swapfile bs=1M count=1024
 mkswap /swapfile
 swapon /swapfile
 
-# 执行第一个命令，并将结果保存到 /tmp/result_rhg
-bash <(curl -L -s check.unlock.media) <<< '4' | tee /tmp/result_rhg
+# 创建或清空结果文件
+> /tmp/result_rhg
+
+# 使用 script 记录第一个命令的输出，并追加到文件
+script -q -c "bash <(curl -L -s check.unlock.media) <<< '4'" /tmp/result_rhg
 
 # 添加彩色文本到文件
 echo -e "\n\033[1;31m[三网回程线路测试]\033[0m\n" >> /tmp/result_rhg
 
-# 执行第二个命令，假设它支持彩色输出，并追加到文件
-curl -L raw.githubusercontent.com/zhanghanyun/backtrace/main/install.sh -sSf | sh | tee -a /tmp/result_rhg
+# 使用 script 记录第二个命令的输出，并追加到文件
+script -q -c "curl -L raw.githubusercontent.com/zhanghanyun/backtrace/main/install.sh -sSf | sh" /tmp/result_rhg
 
 # 添加彩色文本到文件
 echo -e "\n\033[1;31m[YABS测试]\033[0m\n" >> /tmp/result_rhg
 
-# 执行第三个命令，并追加到文件
-curl -sL yabs.sh | bash -s -- -i | tee -a /tmp/result_rhg
+# 使用 script 记录第三个命令的输出，并追加到文件
+script -q -c "curl -sL yabs.sh | bash -s -- -i" /tmp/result_rhg
 
 # 添加彩色文本到文件
 echo -e "\n\033[1;31m[三网单线程速度测试]\033[0m\n\nRemoval Flag" >> /tmp/result_rhg
 
-# 执行第四个命令，并追加到文件
-bash <(curl -s -L raw.githubusercontent.com/i-abc/Speedtest/main/speedtest.sh | sed '/clear/d') <<< '2' | tee -a /tmp/result_rhg
+# 使用 script 记录第四个命令的输出，并追加到文件
+script -q -c "bash <(curl -s -L raw.githubusercontent.com/i-abc/Speedtest/main/speedtest.sh | sed '/clear/d') <<< '2'" /tmp/result_rhg
 
 # 删除文件中的特定部分
 sed -i '/AD No.1/,/即日起老用户下单买一个月送一个月/d' /tmp/result_rhg
